70d273dd75964eb9a07afdc79c2c6c95489098ac
DeerOx2994 <oksusu.deerox@gmail.com>
2019-12-18 10:47:40 +0000

    Updates

---
+++ precache-manifest.52a62f0ff5cca98b0272ad377838a6c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1ea6bb542b4d7711949d8be36afcdc",
+    "url": "/deerox_lab/index.html"
+  },
+  {
+    "revision": "8554042cd08e7cddc66d",
+    "url": "/deerox_lab/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ae51a7294cf956a9cc97",
+    "url": "/deerox_lab/static/js/2.b2c77bcf.chunk.js"
+  },
+  {
+    "revision": "8554042cd08e7cddc66d",
+    "url": "/deerox_lab/static/js/main.f20316d8.chunk.js"
+  },
+  {
+    "revision": "694c3ec97363f21463d7",
+    "url": "/deerox_lab/static/js/runtime-main.d9bfe1ec.js"
+  }
+]);
